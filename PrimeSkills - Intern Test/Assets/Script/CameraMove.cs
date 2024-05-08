77ac890579e3eeea0c50d8c6a19ab9d94c0a71d4
Diaz Pramudhito <56389630+diazpsp@users.noreply.github.com>
2024-05-08 14:33:26 +0700

    Final Updates
    Did the bonus task

--- PrimeSkills - Intern Test/Assets/Script/CameraMove.cs
+++ PrimeSkills - Intern Test/Assets/Script/CameraMove.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    public GameObject tutorialPanel;
     public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tutorialPanel = GameObject.Find("Tutorial Main Panel");
     }
 
     void Update()
     {
-        transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        if (!tutorialPanel.activeInHierarchy)
+        {
+            transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        }
+        else{
+
+        }
+        
     }
 }
